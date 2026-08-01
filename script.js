d5a59fc64a8dd6029f3055509dee75a72d9547cb
shafiul <rayankhan64786@gmail.com>
2026-08-02 01:28:57 +0530

    Update portfolio website

--- script.js
+++ script.js
@@ -37,3 +37,23 @@ function type() {
 }
 
 type();
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 300) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+
+});
+
+topBtn.addEventListener("click", () => {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
